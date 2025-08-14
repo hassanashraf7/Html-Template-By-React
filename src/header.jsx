c738812adc66538a82b9ed7a69fd5bd70032cf5b
Muhammad Hassan <hassandesigner31@gmail.com>
2025-08-14 19:29:24 +0500

    add navigation bar

--- src/header.jsx
+++ src/header.jsx
@@ -8,11 +8,11 @@ const Header = () => {
           <li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
           <li><a href="/services">Services</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><a href="/contact">Contact Us</a></li>
         </ul>
       </nav>
     </div>
   );
 };
 
-export default Header;
+export default Header; 
